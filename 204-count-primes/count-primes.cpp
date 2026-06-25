a1a7ff0a4ace1014d2ff9927b8eb7ec5e732d21c
Aditya Sekhar Das <144005527+AdityaSekharDas@users.noreply.github.com>
2026-06-25 11:23:26 +0530

    Time: 151 ms (88.88%) | Memory: 132.7 MB (8.45%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -3,9 +3,9 @@ public:
     int countPrimes(int n) {
         if(n < 2) return 0;
         vector<int> p(n+1,1);
-        for(int i=2; i<=n; i++) {
+        for(int i=2; i*i<=n; i++) {
             if(p[i] == 1) {
-                for(int j=2*i; j<=n; j+=i) p[j] = 0;
+                for(int j=i*i; j<=n; j+=i) p[j] = 0;
             }
         }
         int cnt = 0;
