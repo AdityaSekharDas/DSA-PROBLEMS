ce3c2e63882b9f3c6b300c31d315faf16defa5b2
Aditya Sekhar Das <144005527+AdityaSekharDas@users.noreply.github.com>
2026-07-13 15:34:47 +0530

    Time: 0 ms (100.00%) | Memory: 69.6 MB (60.94%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -5,13 +5,14 @@ public:
         int len = 0 ,  l = 0 , zero = 0;
         for(int r=0; r<n; r++) {
             if(nums[r] == 0) zero++;
+            if(zero <= k) len = max(len,r-l+1);
             while(zero > k) {
                 if(nums[l] == 0) {
                     zero--;
                 }
                 l++;
             }            
-            if(zero <= k) len = max(len,r-l+1);
+            
         }
         return len;
     }
