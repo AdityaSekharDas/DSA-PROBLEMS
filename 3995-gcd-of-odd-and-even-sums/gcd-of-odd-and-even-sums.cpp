9e833c455a5dd023f2b62fdc620c48f73495beba
Aditya Sekhar Das <144005527+AdityaSekharDas@users.noreply.github.com>
2026-07-15 09:35:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int sumodd = 0 , sumeven = 0;
+        for(int i=1; i<=n; i++) {
+            sumodd += 2 * i - 1;
+            sumeven += 2 * i;
+        }
+        return gcd(sumodd,sumeven);
+    }
+};
