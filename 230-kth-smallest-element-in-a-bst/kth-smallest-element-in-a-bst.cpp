57cddb45edd9415deb23fe5da32a661d9806a130
Aditya Sekhar Das <144005527+AdityaSekharDas@users.noreply.github.com>
2026-08-29 16:27:39 +0530

    Time: 0 ms (100.00%) | Memory: 24.7 MB (7.22%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -13,20 +13,16 @@ class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
         vector<int> ans;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while(!q.empty()) {
-            TreeNode* node = q.front();
-            q.pop();
-
-            ans.push_back(node->val);
-            if(node->left) q.push(node->left);
-            if(node->right) q.push(node->right);
-        }
-
-        sort(ans.begin(),ans.end());
+        inorder(root,ans);
         return ans[k-1];
     }
+
+    void inorder(TreeNode* root, vector<int>& ans) {
+        if(root == NULL) return;
+
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+    }
+    
 };
